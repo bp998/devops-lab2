a0e219c323f9e7039cc0a45ac84e1e63f1b1bf6c
Twoje Imie <twoj@email.com>
2025-12-23 13:32:55 +0100

    python web serwis init

---
+++ app.py
@@ -0,0 +1,11 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Hello DevOps! To moja aplikacja na Lab2.")
+
+print("Serwer rusza na porcie 8080...")
+HTTPServer(('0.0.0.0', 8080), MyHandler).serve_forever()
