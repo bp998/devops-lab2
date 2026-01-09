e7318ec76d8bea4caf3742ff7a2d459646d536f7
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2026-01-09 12:41:36 +0100

    adding Dockerfile

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ class MyHandler(SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write("Hello world.")
+        self.wfile.write("Hello world.".encode('utf-8'))
 
-print("Serwer rusza na porcie 8080...")
+if __name__ == "__main__":
+    print("Serwer rusza na porcie 8080...")
 HTTPServer(('0.0.0.0', 8080), MyHandler).serve_forever()
